30dd1a5efca2a20288e33dc0dce08614199dafad
louispaulet <louispaulet13@gmail.com>
2023-10-09 23:40:47 +0200

    remove EOL chars and extract only html code from chatgpt response

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, request, render_template_string
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
+import re
 
 app = Flask(__name__)
+
+def extract_content_between_backticks(input_string):
+    # Define the regular expression pattern
+    pattern = r'```(.*?)```'
+
+    # Use re.findall to find all matches of the pattern in the input string
+    matches = re.findall(pattern, input_string, re.DOTALL)
+
+    # get the first match
+    first_match = matches[0]
+    
+    #remove EOL for html display in chatGPT web interface
+    first_match = first_match.replace(r"\n", '')
+    
+    # do not return "html" (skip first 4 chars)
+    return first_match[4:]
+
 
 # Load OpenAI API key from file
 with open('api_key.txt', 'r') as file:
@@ -30,8 +48,8 @@ def generate_page():
     <h1>You clicked: {link_text}</h1>
     <p>Generated response: {response}</p>
     """
-    
-    return page_content
+    print(page_content)
+    return extract_content_between_backticks(page_content)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
